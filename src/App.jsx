f9253d66666bc6d2c6074cfe2d0088350263bbae
ZiglaCity <ziglacity@gmail.com>
2025-02-27 06:19:51 +0000

    Setting up components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import TextInputSection from './components/TextInputSection';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [inputText, setInputText] = useState("");
+  const [rotType, setRotType] = useState(13); 
+  const [mode, setMode] = useState("encrypt"); 
+  const [outputText, setOutputText] = useState("");
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <TextInputSection
+          inputText={inputText}
+          setInputText={setInputText}
+          mode={mode}
+          setmMode={setMode}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/TextInputSection.jsx
@@ -0,0 +1,21 @@
+function TextInputSection( {inputText, setInputText, mode, setmMode }){
+    return (
+        <>
+            <div>
+                <select value={mode} onChange={(e) => setmMode(e.target.value)}>
+                    <option value="encrypt">Encrypt</option>
+                    <option value="decrypt">Decrypt</option>
+                </select>
+
+                <textarea 
+                    value={inputText}
+                    onChange={(e) => setInputText(e.target.value)}
+                    placeholder="Enter text to encrypt or decrypt..."
+                />
+            </div>
+        </>
+    )
+
+}
+
+export default TextInputSection;
