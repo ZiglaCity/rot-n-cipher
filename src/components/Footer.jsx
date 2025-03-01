4c9f4d1f8feb6ac7bf4f94c061c563d62512ea1a
ZiglaCity <ziglacity@gmail.com>
2025-03-01 17:37:00 +0000

    Created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer style={{ textAlign: "center", padding: "10px", borderTop: "1px solid #ccc", marginTop: "20px" }}>
+            <p>© ZiglaCity 2025</p>
+            <p>🔒 Encrypted Message: "Uryyb Jbeyq!" (Hint: ROT-13 😉)</p>
+        </footer>
+    );
+}
+
+export default Footer;
